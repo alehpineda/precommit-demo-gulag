d60a79b99ec3d9a40fe7552a4236c6800b9d9200
alehpineda <ale.hpineda@gmail.com>
2023-07-14 21:58:21 -0500

    black

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from typing import List
 import sys
-    
+
+
 def hello_world():
     return "Hola Mundo"
-   
+
+
 if __name__ == "__main__":
-   hello_world()   
-   hello_world()
+    hello_world()
